c25edffffb27a7d825f7c7ce5cf047f3c3661ab2
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-02-27 11:57:19 +0000

    Implement new Home and Favorites views
    Added new Home view that includes movie listing and slide show. Configured device-startup to load movies for different categories. Also created Favorites view template for later implementation. Added navigation functionality to CustomBottomNavigation to set currents index and handle navigation flow. Package exports for new views and route updates were made as well.

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,21 +1,68 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/views/home_views/favorites_view.dart';
+import 'package:cinemapedia/presentation/views/home_views/home_view.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+
+
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
         GoRoute(
-            path: 'movie/:id',
-            name: MovieScreen.name,
-            builder: (context, state) {
-              final movieId = state.pathParameters['id'] ?? 'no-id';
+            path: '/',
+          builder: (context, state) {
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
 
-              return MovieScreen(
-                movieId: movieId,
-              );
-            }),
-      ]),
-]);
+                return MovieScreen(
+                  movieId: movieId,
+                );
+              },
+            ),
+          ],
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        ),
+
+      ],
+    )
+
+
+    // ROUTES PARENT CHILD - EXAMPLE
+    // GoRoute(
+    //   path: '/',
+    //   name: HomeScreen.name,
+    //   builder: (context, state) => const HomeScreen(
+    //     childView: HomeView(),
+    //   ),
+    //   routes: [
+    //     GoRoute(
+    //       path: 'movie/:id',
+    //       name: MovieScreen.name,
+    //       builder: (context, state) {
+    //         final movieId = state.pathParameters['id'] ?? 'no-id';
+    //
+    //         return MovieScreen(
+    //           movieId: movieId,
+    //         );
+    //       },
+    //     ),
+    //   ],
+    // ),
+  ],
+);
